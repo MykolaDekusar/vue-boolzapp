301369cb2eed3244ca83afc6857ee0f473051f51
MykolaDekusar <ndekusar97@gmail.com>
2024-05-24 11:57:45 +0200

    Milestone 4 completed

---
+++ vue-boolzapp-milestone4/scripts/main.js
@@ -0,0 +1,218 @@
+"use strict";
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      contacts: [
+        {
+          name: "Michele",
+          avatar: "./img/avatar_1.jpg",
+          visible: true,
+          messages: [
+            {
+              date: "10/01/2020 15:30:55",
+              message: "Hai portato a spasso il cane?",
+              status: "sent",
+            },
+            {
+              date: "10/01/2020 15:50:00",
+              message: "Ricordati di stendere i panni",
+              status: "sent",
+            },
+            {
+              date: "10/01/2020 16:15:22",
+              message: "Tutto fatto!",
+              status: "received",
+            },
+          ],
+        },
+        {
+          name: "Fabio",
+          avatar: "./img/avatar_2.jpg",
+          visible: true,
+          messages: [
+            {
+              date: "20/03/2020 16:30:00",
+              message: "Ciao come stai?",
+              status: "sent",
+            },
+            {
+              date: "20/03/2020 16:30:55",
+              message: "Bene grazie! Stasera ci vediamo?",
+              status: "received",
+            },
+            {
+              date: "20/03/2020 16:35:00",
+              message: "Mi piacerebbe ma devo andare a fare la spesa.",
+              status: "sent",
+            },
+          ],
+        },
+        {
+          name: "Samuele",
+          avatar: "./img/avatar_3.jpg",
+          visible: true,
+          messages: [
+            {
+              date: "28/03/2020 10:10:40",
+              message: "La Marianna va in campagna",
+              status: "received",
+            },
+            {
+              date: "28/03/2020 10:20:10",
+              message: "Sicuro di non aver sbagliato chat?",
+              status: "sent",
+            },
+            {
+              date: "28/03/2020 16:15:22",
+              message: "Ah scusa!",
+              status: "received",
+            },
+          ],
+        },
+        {
+          name: "Alessandro B.",
+          avatar: "./img/avatar_4.jpg",
+          visible: true,
+          messages: [
+            {
+              date: "10/01/2020 15:30:55",
+              message: "Lo sai che ha aperto una nuova pizzeria?",
+              status: "sent",
+            },
+            {
+              date: "10/01/2020 15:50:00",
+              message: "Si, ma preferirei andare al cinema",
+              status: "received",
+            },
+          ],
+        },
+        {
+          name: "Alessandro L.",
+          avatar: "./img/avatar_5.jpg",
+          visible: true,
+          messages: [
+            {
+              date: "10/01/2020 15:30:55",
+              message: "Ricordati di chiamare la nonna",
+              status: "sent",
+            },
+            {
+              date: "10/01/2020 15:50:00",
+              message: "Va bene, stasera la sento",
+              status: "received",
+            },
+          ],
+        },
+        {
+          name: "Claudia",
+          avatar: "./img/avatar_6.jpg",
+          visible: true,
+          messages: [
+            {
+              date: "10/01/2020 15:30:55",
+              message: "Ciao Claudia, hai novità?",
+              status: "sent",
+            },
+            {
+              date: "10/01/2020 15:50:00",
+              message: "Non ancora",
+              status: "received",
+            },
+            {
+              date: "10/01/2020 15:51:00",
+              message: "Nessuna nuova, buona nuova",
+              status: "sent",
+            },
+          ],
+        },
+        {
+          name: "Federico",
+          avatar: "./img/avatar_7.jpg",
+          visible: true,
+          messages: [
+            {
+              date: "10/01/2020 15:30:55",
+              message: "Fai gli auguri a Martina che è il suo compleanno!",
+              status: "sent",
+            },
+            {
+              date: "10/01/2020 15:50:00",
+              message: "Grazie per avermelo ricordato, le scrivo subito!",
+              status: "received",
+            },
+          ],
+        },
+        {
+          name: "Davide",
+          avatar: "./img/avatar_8.jpg",
+          visible: true,
+          messages: [
+            {
+              date: "10/01/2020 15:30:55",
+              message: "Ciao, andiamo a mangiare la pizza stasera?",
+              status: "received",
+            },
+            {
+              date: "10/01/2020 15:50:00",
+              message: "No, l'ho già mangiata ieri, ordiniamo sushi!",
+              status: "sent",
+            },
+            {
+              date: "10/01/2020 15:51:00",
+              message: "OK!!",
+              status: "received",
+            },
+          ],
+        },
+      ],
+      //inizializzo index degli utenti
+      userIndex: 0,
+      //inizializzo mex utente
+      userMessage: "",
+      userSearch: "",
+    };
+  },
+  methods: {
+    changeUser(index) {
+      this.userIndex = index;
+      this.messagesNumber = this.contacts[this.userIndex].messages.length;
+    },
+    sendUserText() {
+      if (this.userMessage) {
+        this.contacts[this.userIndex].messages.push({
+          date: "21",
+          message: this.userMessage,
+          status: "sent",
+        });
+        this.userMessage = "";
+
+        setTimeout(() => {
+          answerIa(this.contacts, this.userIndex);
+        }, 1000);
+
+        function answerIa(contacts, user) {
+          contacts[user].messages.push({
+            date: "",
+            message: "Ok",
+            status: "received",
+          });
+        }
+      }
+    },
+    searchContact() {
+      this.contacts.forEach((contact) => {
+        if (
+          contact.name.toLowerCase().includes(this.userSearch.toLowerCase())
+        ) {
+          contact.visible = true;
+        } else contact.visible = false;
+      });
+    },
+  },
+}).mount("#app");
+
+//time formatting
+//const myTest = dt.now().setLocale('it').toFormat('D T');
